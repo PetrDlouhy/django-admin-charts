a1ade4e21483d4c0f5ffda60acdd27e78cbb4e80
Santiago Benitez <santiago9607b@gmail.com>
2024-04-12 20:35:33 -0500

    Change default language for demo app

--- demoproject/demoproject/settings.py
+++ demoproject/demoproject/settings.py
@@ -76,7 +76,7 @@ TIME_ZONE = "America/Chicago"
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
-LANGUAGE_CODE = "en-us.UTF-8"
+LANGUAGE_CODE = "en-us"
 
 SITE_ID = 1
 
